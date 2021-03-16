87d2c46d51681ca24c6e20fbc088bee67e1e9b79
dmcaa-iscte <79224975+dmcaa-iscte@users.noreply.github.com>
2021-03-16 05:49:50 -1200

    Update exemplo.java

--- demo/src/demo/exemplo.java
+++ demo/src/demo/exemplo.java
@@ -3,8 +3,8 @@ package demo;
 public class exemplo {
  
 	public static void main(String[] args) {
-		System.out.println("Ol· Git");
-
+		System.out.println("Ol√° Git");
+		System.out.println("Alterei pelo git");
 	}
 
 }
